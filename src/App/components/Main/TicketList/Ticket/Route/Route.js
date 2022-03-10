f878505263242901a35c4d478c4872a5d1f3dd00
Alexander Khomenko <aleksandr.kh.galanix@gmail.com>
2022-03-11 00:00:20 +0200

    modified: choiceTransfersHeadline in Route; add: formatted time, calculateArrivalTime

--- src/App/components/Main/TicketList/Ticket/Route/Route.js
+++ src/App/components/Main/TicketList/Ticket/Route/Route.js
@@ -1,44 +1,55 @@
 import style from "./Route.module.css";
 
 function Route({info}) {
+    const formattedTime = calculateArrivalTime(info.date, info.duration);
+    function calculateArrivalTime(departureString, duration) {
+        let date = new Date( Date.parse(departureString) );
+        const startHours = date.getHours();
+        const startMinutes = date.getMinutes();
+        date.setMinutes(date.getMinutes() + duration);
+        const endHours = date.getHours();
+        const endMinutes = date.getMinutes();
+        return {
+            startTime: `${startHours}:${startMinutes}`,
+            endTime: `${endHours}:${endMinutes}`
+        }
+    } 
     function convertMinutesToHoursMin(min) {
-        const hours = Math.floor(totalMinutes / 60);          
-        const minutes = totalMinutes % 60;
+        let hours = Math.floor(min / 60);          
+        let minutes = min % 60;
 
         hours = hours < 10 ? '0' + hours : hours; 
         minutes = minutes < 10 ? '0' + minutes : minutes; 
         return `${hours}ч ${minutes}м`;
-        // return `${hours} `
     }
-    function calculateTransfersHeadline(transfers) {
+    function choiceTransfersHeadline(transfers) {
         let headline = '';
         switch (transfers) {
             case 0:
                 headline = "Без пересадок"
                 break;
-            case 0:
-                headline = "Без пересадок"
-                break;
-            case 0:
-                headline = "Без пересадок"
+            case 1:
+                headline = "1 пересадка"
                 break;
             default:
+                headline = `${transfers} пересадки`
                 break;
         }
+        return headline;
     }
     return (
         <ul>
         <li className={style.route}>
             <div className={style.headline}>{info.origin} – {info.destination}</div>
-            <div className={style.info}>10:45 – 08:00</div>
+            <div className={style.info}>{formattedTime.startTime} – {formattedTime.endTime}</div>
         </li>
         <li className={style.allTime}>
             <div className={style.headline}>В пути</div>
             <div className={style.info}>{convertMinutesToHoursMin(info.duration)}</div>
         </li>
         <li className={style.transfers}>
-            <div className={style.headline}>2 пересадки</div>
-            <div className={style.info}>HKG, JNB</div>
+            <div className={style.headline}>{choiceTransfersHeadline(info.stops.length)}</div>
+            <div className={style.info}>{info.stops.toString()}</div>
         </li>
     </ul>
     );
