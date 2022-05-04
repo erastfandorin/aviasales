9c7556c5278ea3324f8c4e8844e909dfd4423a3d
Alexander Khomenko <aleksandr.kh.galanix@gmail.com>
2022-05-04 22:11:48 +0300

    add: react-toastify

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,9 @@ import TransferFilter from "./components/Main/TransferFilter/TransferFilter.js";
 import PreferenceTabs from "./components/Main/PreferenceTabs/PreferenceTabs.js";
 import TicketList from "./components/Main/TicketList/TicketList.js";
 
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css';
+
 import ticketsService from "./services/ticketsService.js";
 
 import Logo from "../img/Logo.png";
@@ -13,105 +16,144 @@ function App() {
   const [preference, setPreference] = useState("optimal"); // Cheapest, Fastest, Optimal
   const transferCountDefault = -1;
   const [transferCount, setTransferCount] = useState(transferCountDefault); // -1, 0, 1, 2, 3, 100
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const [tickets, setTickets] = useState([]);
   const [filteredTickets, setFilteredTickets] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     (async () => {
       const result = await ticketsService.getAllTickets();
+      console.log(result);
       setTickets(result);
-    })()
-    setIsLoading(false);
+      setIsLoading(false);
+    })();
   }, []);
 
   useEffect(() => {
-    const filteredTransferTickets = tickets.filter( ticket => {
-      return (ticket.segments[0].stops.length <= transferCount
-      && ticket.segments[1].stops.length <= transferCount) 
-      || transferCount === transferCountDefault;
-    })
+    const filteredTransferTickets = tickets.filter((ticket) => {
+      return (
+        (ticket.segments[0].stops.length <= transferCount &&
+          ticket.segments[1].stops.length <= transferCount) ||
+        transferCount === transferCountDefault
+      );
+    });
 
     let sortedTickets;
     switch (preference) {
-      case 'cheapest':
-        sortedTickets = filteredTransferTickets.sort( function ( a, b ) {
+      case "cheapest":
+        sortedTickets = filteredTransferTickets.sort(function (a, b) {
           return a.price - b.price;
         });
         break;
-      case 'fastest':
-        sortedTickets = filteredTransferTickets.sort( function ( a, b ) {
-          const sumTicketDurationA = a.segments.reduce( (previousValue, currentValue) => 
-            previousValue + currentValue.duration, 0);
-          const sumTicketDurationB = b.segments.reduce( (previousValue, currentValue) =>
-            previousValue + currentValue.duration, 0);
-          
+      case "fastest":
+        sortedTickets = filteredTransferTickets.sort(function (a, b) {
+          const sumTicketDurationA = a.segments.reduce(
+            (previousValue, currentValue) =>
+              previousValue + currentValue.duration,
+            0
+          );
+          const sumTicketDurationB = b.segments.reduce(
+            (previousValue, currentValue) =>
+              previousValue + currentValue.duration,
+            0
+          );
+
           return sumTicketDurationA - sumTicketDurationB;
         });
         break;
-      case 'optimal':
-        const prices = filteredTransferTickets.map(ticket => ticket.price)
+      case "optimal":
+        const prices = filteredTransferTickets.map((ticket) => ticket.price);
         const maxPrice = Math.max(...prices, 0);
         const minPrice = Math.min(...prices, 0);
-        
-        const durations = filteredTransferTickets.map(ticket => {
-          return ticket.segments.reduce( (previousValue, currentValue) => 
-          previousValue + currentValue.duration, 0);
+
+        const durations = filteredTransferTickets.map((ticket) => {
+          return ticket.segments.reduce(
+            (previousValue, currentValue) =>
+              previousValue + currentValue.duration,
+            0
+          );
         });
         const maxDuration = Math.max(...durations, 0);
         const minDuration = Math.min(...durations, 0);
 
-        sortedTickets = filteredTransferTickets.sort( function (a, b) {
-          const normalizedPriceA = normalizeCount(a.price , maxPrice, minPrice);
-          const normalizedPriceB = normalizeCount(b.price , maxPrice, minPrice);
-  
-          const durationA = a.segments.reduce( (previousValue, currentValue) => 
-            previousValue + currentValue.duration, 0)
-          const durationB = b.segments.reduce( (previousValue, currentValue) => 
-            previousValue + currentValue.duration, 0)
-          const normalizedDurationA = normalizeCount(durationA, maxDuration, minDuration);
-          const normalizedDurationB = normalizeCount(durationB, maxDuration, minDuration);
-  
+        sortedTickets = filteredTransferTickets.sort(function (a, b) {
+          const normalizedPriceA = normalizeCount(a.price, maxPrice, minPrice);
+          const normalizedPriceB = normalizeCount(b.price, maxPrice, minPrice);
+
+          const durationA = a.segments.reduce(
+            (previousValue, currentValue) =>
+              previousValue + currentValue.duration,
+            0
+          );
+          const durationB = b.segments.reduce(
+            (previousValue, currentValue) =>
+              previousValue + currentValue.duration,
+            0
+          );
+          const normalizedDurationA = normalizeCount(
+            durationA,
+            maxDuration,
+            minDuration
+          );
+          const normalizedDurationB = normalizeCount(
+            durationB,
+            maxDuration,
+            minDuration
+          );
+
           const normalizedCountA = normalizedPriceA + normalizedDurationA;
           const normalizedCountB = normalizedPriceB + normalizedDurationB;
 
-          function normalizeCount(val, max, min) { 
-            return (val - min) / (max - min); 
+          function normalizeCount(val, max, min) {
+            return (val - min) / (max - min);
           }
 
           return normalizedCountA - normalizedCountB;
-        }); 
+        });
         break;
       default:
         break;
     }
     const sortedFiveTickets = sortedTickets.slice(0, 5);
     setFilteredTickets(sortedFiveTickets);
-  }, [tickets, preference, transferCount, transferCountDefault])
-  
+  }, [tickets, preference, transferCount, transferCountDefault]);
+
   return (
     <>
       <header className={styles.header}>
         <img src={Logo} alt="logo" />
       </header>
       <main className={styles.main}>
-        <TransferFilter 
-          setTransferCount={setTransferCount} 
+        <TransferFilter
+          setTransferCount={setTransferCount}
           transferCountDefault={transferCountDefault}
         />
         <div className={styles.mainRight}>
-          <PreferenceTabs 
-            preference={preference} 
+          <PreferenceTabs
+            preference={preference}
             setPreference={setPreference}
           />
-          {isLoading
-            ? <div className={styles.loadingSpinner}>
-                <div className={styles.load}>
-                  <div></div>
-                </div>
-              </div> 
-            : <TicketList tickets={filteredTickets} /> }
+          {isLoading ? (
+            <div className={styles.loadingSpinner}>
+              <div className={styles.load}>
+                <div></div>
+              </div>
+            </div>
+          ) : (
+            <TicketList tickets={filteredTickets} />
+          )}
         </div>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
       </main>
     </>
   );
--- src/App/services/ticketsService.js
+++ src/App/services/ticketsService.js
@@ -1,43 +1,52 @@
+import { toast } from "react-toastify";
+
 const ticketsService = {
-    URL: 'https://front-test.beta.aviasales.ru',
-    searchId: '',
+  URL: "https://front-test.beta.aviasales.ru",
+  searchId: "",
 
-    getSearchId: async function() {
-        try {
-            const response = await fetch(`${this.URL}/search`);
-            const parsedResponse = await response.json();
-            this.searchId = parsedResponse.searchId;
-        } catch (err) {
-            throw err
-        }
-    },
-    getPackTickets: async function() {
-        try {
-            const response = await fetch(`${this.URL}/tickets?searchId=${this.searchId}`);
-            if(response.status === 200) {
-                const parsedResponse = await response.json();
-                return parsedResponse;
-            }
-            return { tickets: [], stop: false };
-        } catch (err) {
-            throw err
-        }
-
-    },
-    getAllTickets: async function() {
-        try {
-            let ticketPack = [];
-            let response;
-            await this.getSearchId();
-            do {
-                response = await this.getPackTickets();
-                ticketPack = [ ...ticketPack, ...response.tickets];
-            } while (!response.stop);
-            return ticketPack
-        } catch (err) {
-            throw err
-        }
-    },
-}
+  getSearchId: async function () {
+    try {
+      console.log(`${this.URL}/search`);
+      const response = await fetch(`${this.URL}/search`);
+      const parsedResponse = await response.json();
+      this.searchId = parsedResponse.searchId;
+    } catch (err) {
+      toast.error("Some trouble on server, sorry(");
+      throw err;
+    }
+  },
+  getPackTickets: async function () {
+    try {
+      console.log(`${this.URL}/tickets?searchId=${this.searchId}`);
+      const response = await fetch(
+        `${this.URL}/tickets?searchId=${this.searchId}`
+      );
+      if (response.status === 200) {
+        const parsedResponse = await response.json();
+        return parsedResponse;
+      }
+      return { tickets: [], stop: false };
+    } catch (err) {
+      toast.error("Some trouble on server, sorry(");
+      throw err;
+    }
+  },
+  getAllTickets: async function () {
+    try {
+      let ticketPack = [];
+      let response;
+      await this.getSearchId();
+      console.log("active?");
+      do {
+        response = await this.getPackTickets();
+        ticketPack = [...ticketPack, ...response.tickets];
+      } while (!response.stop);
+      return ticketPack;
+    } catch (err) {
+      toast.error("Some trouble on server, sorry(");
+      throw err;
+    }
+  },
+};
 
 export default ticketsService;
